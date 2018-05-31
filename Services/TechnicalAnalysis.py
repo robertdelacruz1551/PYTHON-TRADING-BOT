aa19d9beb1d85fe4d8cd5ab07415cd6128a495bd
Rob <Rob@HP-Laptop.local>
2018-05-31 16:13:19 -0400

    cleaned up technical analysis

--- Services/TechnicalAnalysis.py
+++ Services/TechnicalAnalysis.py
@@ -10,19 +10,14 @@ class TechnicalAnalyzer():
         self.order_book = None
         self.ticker     = None
         self.config     = config
-        # self.livePrice  = False
 
     def OHLC(self, data):
         self.candles = pd.DataFrame(data=data, columns=(['time', 'low', 'high', 'open', 'close', 'volume'])).iloc[::-1]
-        # if self.livePrice and self.ticker and 'price' in self.ticker:
-        #     self.candles.loc[0, 'close'] = float(self.ticker['price'])
         stock = StockDataFrame.retype(self.candles)
         if 'rsi_period' in self.config:
             self.candles['rsi'] = stock['rsi_{}'.format(self.config['rsi_period'])]
-            self.candles['rsi_prior'] = self.candles['rsi'].shift(1)
         if 'cci_period' in self.config:
             self.candles['cci'] = stock['cci_{}'.format(self.config['cci_period'])]
-            self.candles['cci_prior'] = self.candles['cci'].shift(1)
         if 'rolling_period' in self.config:
             self.candles['middle_band'] = self.candles['close'].rolling(self.config['rolling_period']).mean()
             self.candles['upper_band']  = self.candles['middle_band'] + self.candles['close'].rolling(self.config['rolling_period']).std() * self.config['upper_band_deviations']
